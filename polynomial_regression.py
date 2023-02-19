10e9095d7b4d3f47d1ae69283d34dfe918894e8d
Rylan <rylanschaeffer@gmail.com>
2023-02-19 11:29:13 -0800

    Checkpointing polynomial regression before switching to Legendre polynomials

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import scipy.special
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
@@ -17,7 +18,7 @@ sns.set_style("whitegrid")
 np.random.seed(0)
 
 
-num_data_list = [15]
+num_data_list = [20]
 num_features_list = [1, 2, 3, 4, 5, 10, 15, 20, 40, 60]
 num_repeat_list = np.arange(5).tolist()
 
@@ -25,21 +26,25 @@ results_dir = 'results/polynomial_regression'
 os.makedirs(results_dir, exist_ok=True)
 
 # Create sklearn linear regression object
-regr = linear_model.LinearRegression(fit_intercept=False)
+regr = linear_model.LinearRegression(fit_intercept=True)
 
-low, high = -4., 4.
+def compute_y_from_x(X: np.ndarray):
+    return np.add(2. * X, np.cos(X * 25))[:, 0]
+
+
+low, high = -1., 1.
 for num_data in num_data_list:
 
-    # Sample data from between -1 and 1.
+    # Sample data from between low and high.
     X_train = np.random.uniform(low=low, high=high, size=(num_data, 1))
-    X_infinite = np.linspace(low, high, 1000).reshape(-1, 1)
-    y = np.cos(X_train)[:, 0]
-    y_infinite = np.cos(X_infinite)[:, 0]
+    X_test = np.linspace(low, high, 1000).reshape(-1, 1)
+    y_train = compute_y_from_x(X_train)
+    y_test = compute_y_from_x(X_test)
 
     # Plot the data.
     plt.close()
-    sns.lineplot(x=X_infinite[:, 0], y=y_infinite, label='True Function')
-    sns.scatterplot(x=X_train[:, 0], y=y, s=30, color='k', label='Data')
+    sns.lineplot(x=X_test[:, 0], y=y_test, label='True Function')
+    sns.scatterplot(x=X_train[:, 0], y=y_train, s=30, color='k', label='Data')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.savefig(os.path.join(results_dir, f'data.png'),
@@ -48,24 +53,27 @@ for num_data in num_data_list:
     plt.show()
     plt.close()
 
-    # Fit a linear regression model.
-    regr.fit(X_train, y)
-    y_infinite_pred = regr.predict(X_infinite)
+    # # Fit a linear regression model.
+    # regr.fit(X_train, y_train)
+    # y_test_pred = regr.predict(X_test)
 
     for num_features in num_features_list:
 
         # Fit a polynomial regression model.
         poly = PolynomialFeatures(degree=num_features, interaction_only=False)
         X_train_poly = poly.fit_transform(X_train)
-        X_infinite_poly = poly.fit_transform(X_infinite)
-        regr.fit(X_train_poly, y)
-        y_infinite_pred = regr.predict(X_infinite_poly)
+        X_test_poly = poly.fit_transform(X_test)
+        # regr.fit(X_train_poly, y)
+        # if num_features < num_data:
+        beta_hat = np.linalg.pinv(X_train_poly) @ y_train
+        # y_test_pred = regr.predict(X_test_poly)
+        y_test_pred = X_test_poly @ beta_hat
 
         # Plot the polynomial fit data.
         plt.close()
-        sns.lineplot(x=X_infinite[:, 0], y=y_infinite, label='True Function')
-        sns.scatterplot(x=X_train[:, 0], y=y, s=30, color='k', label='Data')
-        sns.lineplot(x=X_infinite[:, 0], y=y_infinite_pred, label=f'Num Param={X_train_poly.shape[1]}')
+        sns.lineplot(x=X_test[:, 0], y=y_test, label='True Function')
+        sns.scatterplot(x=X_train[:, 0], y=y_train, s=30, color='k', label='Data')
+        sns.lineplot(x=X_test[:, 0], y=y_test_pred, label=f'Num Param={X_train_poly.shape[1]}')
         plt.xlabel('x')
         plt.ylabel('y')
         plt.ylim(-1.5, 1.5)
@@ -76,18 +84,18 @@ for num_data in num_data_list:
         plt.close()
 
         # Suppose we had a different realization of data. What would the fit look like?
-        for repeat_idx in range(3):
+        for repeat_idx in range(1):
             X_train_clone = np.random.uniform(low=low, high=high, size=(num_data, 1))
             y_clone = np.cos(X_train_clone)[:, 0]
             X_train_clone_poly = poly.fit_transform(X_train_clone)
             regr.fit(X_train_clone_poly, y_clone)
-            y_infinite_pred_clone = regr.predict(X_infinite_poly)
+            y_test_pred_clone = regr.predict(X_test_poly)
 
             # Plot the polynomial fit data.
             plt.close()
-            sns.lineplot(x=X_infinite[:, 0], y=y_infinite, label='True Function')
+            sns.lineplot(x=X_test[:, 0], y=y_test, label='True Function')
             sns.scatterplot(x=X_train_clone[:, 0], y=y_clone, s=30, color='k', label='Data')
-            sns.lineplot(x=X_infinite[:, 0], y=y_infinite_pred_clone, label=f'Num Param={X_train_poly.shape[1]}')
+            sns.lineplot(x=X_test[:, 0], y=y_test_pred_clone, label=f'Num Param={X_train_poly.shape[1]}')
             plt.xlabel('x')
             plt.ylabel('y')
             plt.ylim(-1.5, 1.5)
